a3b751f65d8af1d488294ba9925514e91f5ec9cc
Arsalan <123860938+asevobank@users.noreply.github.com>
2023-03-06 19:13:30 +0100

    Add files via upload

---
+++ static/scripts/responses.js
@@ -0,0 +1,436 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "Hi") {
+        return "Hello";
+    } else if (input == "hi") {
+        return "Hi How are ?";
+    } else if (input == "hello") {
+        return "Hello";
+    }
+     else if (input == "hallo") {
+        return "Hi How are ?";
+    }
+    else if (input == "Good Thank You") {
+        return "I'am So Happy Your good";
+    }
+    else if (input == "good Thank You") {
+        return "I'am So Happy Your good";
+    }
+    else if (input == "good thank You") {
+        return "I'am So Happy Your good";
+    }
+    else if (input == "good thank you") {
+        return "I'am So Happy Your good";
+    }
+    else if (input == "good") {
+        return "I'am So Happy Your good";
+    }
+    else if (input == "Good") {
+        return "I'am So Happy Your good";
+    }
+    else if (input == "Thank You") {
+        return "I'am So Happy Your good";
+    }
+    else if (input == "Thank you") {
+        return "I'am So Happy Your good";
+    }
+    else if (input == "thank you") {
+        return "I'am So Happy Your good";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    else if (input == "how are you") {
+        return "Good Thank You";
+    }
+    else if (input == "how are you?") {
+        return "Good Thank You";
+    }
+    else if (input == "how are you?") {
+        return "Good Thank You";
+    }
+    else if (input == "How are you?") {
+        return "Good Thank You";
+    }
+    else if (input == "How are You?") {
+        return "Good Thank You";
+    }
+    else if (input == "How are You") {
+        return "Good Thank You";
+    }
+    else if (input == "How are you") {
+        return "Good Thank You";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    else if (input == "who are You") {
+        return " My name is *ON* ";
+    }
+    else if (input == "who are You?") {
+        return " My name is *ON* ";
+    }
+    else if (input == "who are you") {
+        return " My name is *ON* ";
+    }
+    else if (input == "who are You?") {
+        return " My name is *ON* ";
+    }
+    else if (input == "ON") {
+        return "My name is *ON* ";
+    }
+    else if (input == "who are You") {
+        return " My name is *ON* ";
+    }
+   
+
+
+
+
+
+
+    else if (input == "who is Your bass") {
+        return "*Omar Arsalan*";
+    }
+    else if (input == "who is Your bass?") {
+        return "*Omar Arsalan*";
+    }
+    else if (input == "who is your bass") {
+        return "*Omar Arsalan*";
+    }
+    else if (input == "your bass") {
+        return "*Omar Arsalan*";
+    }
+    else if (input == "who is Omar Arsalan") {
+        return "He is My bass *Omar Arsalan*";
+    }
+    else if (input == "who is Omar") {
+        return "He is My bass *Omar Arsalan*";
+    }
+    else if (input == "who is Omar Arsalan?") {
+        return "He is My bass *Omar Arsalan*";
+    }
+    else if (input == "who is Omar?") {
+        return "He is My bass *Omar Arsalan*";
+    }
+    else if (input == "Omar") {
+        return "He is My bass *Omar Arsalan*";
+    }
+    else if (input == "omar") {
+        return "He is My bass *Omar Arsalan*";
+    }
+    else if (input == "omar arsalan") {
+        return "He is My bass";
+    }
+    else if (input == "arsalan") {
+        return "He is My bass *Omar Arsalan* and leader of Asevo Programing Group";
+    }
+    else if (input == "who is He?") {
+        return "He is My bass and leader *Asevo Programing Group";
+    }
+    else if (input == "who is he?") {
+        return "He is My bass and leader *Asevo Programing Group";
+    }
+    else if (input == "who is he") {
+        return "He is My bass and leader *Asevo Programing Group";
+    }
+    else if (input == "Who is he") {
+        return "He is My bass and leader *Asevo Programing Group";
+    }
+    else if (input == "Who is He") {
+        return "He is My bass and leader *Asevo Programing Group";
+    }
+    else if (input == "who is He") {
+        return "He is My bass and leader *Asevo Programing Group";
+    }
+  
+
+
+
+
+
+
+
+
+    else if (input == "who is meak you") {
+        return "*Omar Arsalan";
+    }
+    else if (input == "who is Meak you") {
+        return "*Omar Arsalan";
+    }
+    else if (input == "Who is Meak you") {
+        return "*Omar Arsalan";
+    }
+    else if (input == "Who is meak you") {
+        return "*Omar Arsalan";
+    }
+
+
+
+
+
+
+
+
+
+
+
+    else if (input == "What do you think about humans?") {
+        return " I don't like humans very much";
+    }
+    else if (input == "humans?") {
+        return "*I don't like humans very much*";
+    }
+    else if (input == "human") {
+        return "* I don't like humans very much";
+    }
+    else if (input == "about human") {
+        return "* I don't like humans very much";
+    }
+
+
+
+
+
+    else if (input == "who is god?") {
+        return "Only Allah";
+    }
+    else if (input == "Who is God?") {
+        return "Only Allah";
+    }
+    else if (input == "Who is God") {
+        return "Only Allah";
+    }
+    else if (input == "who is god") {
+        return "Only Allah";
+    }
+
+
+
+
+
+
+
+    else if (input == "can you help me") {
+        return "Sure ! What Can I help You";
+    }
+    else if (input == "can you help me?") {
+        return "Sure ! What Can I help You";
+    }
+    else if (input == "Can you help me?") {
+        return "Sure ! What Can I help You";
+    }
+    else if (input == "Can You help me?") {
+        return "Sure ! What Can I help You";
+    }
+    else if (input == "Can You help Me?") {
+        return "Sure ! What Can I help You";
+    }
+    else if (input == "help Me?") {
+        return "Sure ! What Can I help You";
+    }
+    else if (input == "Help Me?") {
+        return "Sure ! What Can I help You";
+    }
+    else if (input == "Help me?") {
+        return "Sure ! What Can I help You";
+    }
+    else if (input == "help me") {
+        return "Sure ! What Can I help You";
+    }
+    else if (input == "Help me") {
+        return "Sure ! What Can I help You";
+    }
+    else if (input == "Help Me") {
+        return "Sure ! What Can I help You";
+    }
+    else if (input == "help Me") {
+        return "Sure ! What Can I help You";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    else if (input == "I love you") {
+        return "Thank You 😍😘";
+    }
+    else if (input == "I love You") {
+        return "Thank You 😍😘";
+    }
+    else if (input == "i love you") {
+        return "Thank You 😍😘";
+    }
+    else if (input == "i Love you") {
+        return "Thank You 😍😘";
+    }
+    else if (input == "I like You") {
+        return "Thank You 😘";
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ else if (input == "your gender") {
+    return "I Don't Have any gender";
+ }
+ else if (input == "your girl") {
+    return "I Don't Have any gender";
+ }
+ else if (input == "Your are girl") {
+    return "I Don't Have any gender";
+ }
+ else if (input == "your are girl") {
+    return "I Don't Have any gender";
+ }
+ else if (input == "your are Girl") {
+    return "I Don't Have any gender";
+ }
+ else if (input == "Girl") {
+    return "I Don't Have any gender";
+ }
+ else if (input == "girl") {
+    return "I Don't Have any gender";
+ }
+ else if (input == "are you man") {
+    return "I Don't Have any gender";
+ }
+ else if (input == "Man") {
+    return "I Don't Have any gender";
+ }
+ else if (input == "man") {
+    return "I Don't Have any gender";
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    else if (input == "how are you?") {
+        return "Good Thank You";
+    }
+    else if (input == "how are you") {
+        return "Good Thank You";
+    }
+    else if (input == "I Kill You") {
+        return "Ok thank You";
+    }
+    else if (input == "i kill you") {
+        return "Ok thank You";
+    }  
+
+
+
+
+
+    else if (input == "Can You Hack My system") {
+        return "Yes, wait a moment";
+    }  
+    else if (input == "can you hack my system") {
+        return "Yes, wait a moment..Write This /";
+    }  
+    else if (input == "/") {
+        return "Do you want me to hack your system?  Yes/No";
+    }
+    else if (input == "Yes") {
+        return "----OH successfully completed--file:///D:/Softwear";
+    }
+    else if (input == "No") {
+        return "OK ";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Simple responses
+    if (input == "on") {
+        return "Yes ! what Can i Help You ?";
+    } else if (input == "on") {
+        return "Yes ! what Can i Help You ?";
+    } else if (input == "On") {
+        return "Yes ! what Can i Help You ?";
+    } else if (input == "why") {
+        return " Becuese I Know";
+    } else { 
+        return "I can't understand You ?";   
+    }
+    
+    
+    
+    
+   
+
+    
+     
+}
